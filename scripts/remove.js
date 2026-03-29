0311cca1ddadd71a0ec96eb7b4acc1c7e8ab53e2
noob-mishu <mishubarua320@gmail.com>
2026-03-29 21:38:06 +0600

    trying

--- scripts/remove.js
+++ scripts/remove.js
@@ -1,8 +1,29 @@
 document.addEventListener("click", function (e) {
-    if (e.target.classList.contains("remove-btn")) {
+
+    // 1. check if the clicked element has the class "remove-btn"
+    if (e.target.classList.contains("remove-btn"))
+        // 2. if yes, remove the closest parent element with the class "card"
+
+        {
         e.target.closest(".card").remove();
         alert("Job removed successfully!");
+
+        // 3. get the total job element
+        const totalJobElement = document.getElementById("total-job");
+
+        // 4. get the current total job number
+        const currentTotalJob = parseInt(totalJobElement.innerText);
+
+        // 5. decrease the total job number by 1
+        const newTotalJob = currentTotalJob - 1;
+
+        // 6. update the total job element with the new total job number
+        totalJobElement.innerText = newTotalJob;
     }
+
+
+    
+
 
 
     // Othoba Eivabeo Kora Jete Pare
